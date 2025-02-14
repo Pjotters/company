04378d867753f70ed42269b836ae4d6c2523d04b
Pjotters-Chain <05.hek_domein@icloud.com>
2025-02-14 22:53:05 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     // Je bestaande Firebase configuratie
     apiKey: "AIzaSyBCXaYJI9dxwqKD1Qsb_9AOdsnVTPG2uHM",
     authDomain: "pjotters-company.firebaseapp.com",
-    databaseURL: "https://pjotters-company-default-rtdb.europe-west1.firebasedatabase.app",
+    databaseURL: "https://pjotters-company-default-rtdb.europe-west1.firebasedatabase.app/",
     projectId: "pjotters-company",
     storageBucket: "pjotters-company.appspot.com",
     messagingSenderId: "64413422793",
